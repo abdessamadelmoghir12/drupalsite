31e833a5e53e764b9b4ba2f20c3b1cb260ac47f6
abdessamad el moghir <elmoghir42@gmail.com>
2022-07-28 09:26:50 +0100

    Terminer uni 4 : Ajouter article, Ajouter un Block, Ajouter un Logo, Ajouter un module(Admin-Toolbar)

--- web/.ht.router.php
+++ web/.ht.router.php
@@ -24,7 +24,6 @@
  * @see http://php.net/manual/en/features.commandline.webserver.php
  */
 
- 
 $url = parse_url($_SERVER['REQUEST_URI']);
 if (file_exists(__DIR__ . $url['path'])) {
   // Serve the requested resource as-is.
